7efa7c074af7d4da8c12f440d10f3c0434334de8
Davosorio404 <davosorio404@gmail.com>
2021-08-27 02:14:20 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,44 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <title>WORK ONE</title>
+</head>
+<body class="bg-dark text-white">
+    
+    <main>
+
+        <div class="container">
+            <div class="row d-flex justify-content-center text-center">
+                <div class="col-6">
+
+                    <h1 class="mt-3">Points</h1>
+
+                    <hr>
+                    
+                    <a href="first.php"><button type="button" class="btn btn-outline-light w-100 mb-2">First Point</button></a>
+                    <a href="second.php"><button type="button" class="btn btn-outline-light w-100 mb-2">Second Point</button></a>
+                    <a href="third.php"><button type="button" class="btn btn-outline-light w-100 mb-2">Third Point</button></a>
+                    <a href="fourth.php"><button type="button" class="btn btn-outline-light w-100 mb-2">Fourth Point</button></a>
+                    <a href="fifth.php"><button type="button" class="btn btn-outline-light w-100 mb-2">Fifth Point</button></a>
+                    <a href="sixth.php"><button type="button" class="btn btn-outline-light w-100 mb-2">Sixth Point</button></a>
+                    <a href="seventh.php"><button type="button" class="btn btn-outline-light w-100 mb-2">Seventh Point</button></a>
+                    <a href="eighth.php"><button type="button" class="btn btn-outline-light w-100 mb-2">Eighth Point</button></a>
+                    <a href="ninth.php"><button type="button" class="btn btn-outline-light w-100 mb-2">Ninth Point</button></a>
+                    <a href="tenth.php"><button type="button" class="btn btn-outline-light w-100 mb-2">Tenth Point</button></a>
+
+                    <hr>
+
+                </div>
+            </div>
+        </div>
+
+    </main>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+</body>
+</html>
